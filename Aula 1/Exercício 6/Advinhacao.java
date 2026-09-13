5c2aa03a487c8af1ebb6980112cf334d6ce49d9f
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-09-13 16:54:14 -0300

    Create Advinhacao.java

---
+++ Aula 1/Exercício 6/Advinhacao.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Advinhacao {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner entrada = new Scanner(System.in);
+
+        //número gerado pelo pc
+        int num_pc = rand.nextInt(10) + 1;
+
+        System.out.print("Advinhe o número gerado pelo computador: ");
+        //número que o usuário irá entrar
+        int num_user = entrada.nextInt();
+
+        while (num_user != num_pc) {
+            if (num_user > num_pc) {
+                System.out.println("O número que você digitou é maior que o número gerado pelo computador.");
+            } else {
+                System.out.println("O número que você digitou é menor que o número gerado pelo computador.");
+            }
+            System.out.print("Tente novamente: ");
+            num_user = entrada.nextInt();
+        }
+
+        System.out.println("Parabéns! Você advinhou o número!");
+
+        entrada.close();
+    }
+}
