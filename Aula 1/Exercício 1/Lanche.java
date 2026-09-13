b5d80913c7508854242c6432a96a6ba80a9b0016
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-09-13 13:47:21 -0300

    Create Lanche.java

---
+++ Aula 1/Exercício 1/Lanche.java
@@ -0,0 +1,13 @@
+public class Lanche {
+    public static void main(String[] args) {
+        int lanche1 = 10;
+        int lanche2 = 4;
+        int lanche3 = 2;
+        int soma = lanche1 + lanche2 + lanche3;
+        double media = soma / 3.0;
+
+        System.out.println("Número total de lanches: " + soma);
+        System.out.println("Média: " + media);
+    }
+}
+
