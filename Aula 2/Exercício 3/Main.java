df0e5b016ffba4f70bbb41d2c859bd9e89808588
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-09-14 00:48:09 -0300

    Create Main.java

---
+++ Aula 2/Exercício 3/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        Zumbi zumbi1 = new Zumbi();
+        Zumbi zumbi2 = new Zumbi();
+
+        zumbi1.vida = 55;
+        zumbi1.nome = "Alfred";
+
+        zumbi2.vida = 28;
+        zumbi2.nome = "Raymond";
+
+        zumbi1 = zumbi2;
+
+        zumbi1.transfereVida(zumbi2, 10);
+
+        System.out.println("Zumbi 1:");
+        System.out.println("Nome: " + zumbi1.nome);
+        System.out.println("Vida: " + zumbi1.vida);
+
+        System.out.println("\nZumbi 2:");
+        System.out.println("Nome: " + zumbi2.nome);
+        System.out.println("Vida: " + zumbi2.vida);
+    }
+}
