466caf9e8d55212edf2a8fa5b8739526dd0cac61
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-09-15 20:56:54 -0300

    Update ProcessarPedido.java

--- PCMania/src/pcmania/servico/ProcessarPedido.java
+++ PCMania/src/pcmania/servico/ProcessarPedido.java
@@ -4,6 +4,6 @@ import pcmania.modelo.Computador;
 
 public class ProcessarPedido {
     public static void enviarPedido(Computador[] computadores) {
-        System.out.println("Pedido enviado.");
+        System.out.println("Pedido enviado...");
     }
 }
