805c313285d9199b5384df860256ce6ed01b62fe
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-09-15 20:55:23 -0300

    Update MemoriaUSB.java

--- PCMania/src/pcmania/modelo/MemoriaUSB.java
+++ PCMania/src/pcmania/modelo/MemoriaUSB.java
@@ -3,7 +3,7 @@ package pcmania.modelo;
 public class MemoriaUSB {
     private String nome;
     private int capacidade;
-
+    
     public MemoriaUSB(String nome, int capacidade) {
         this.nome = nome;
         this.capacidade = capacidade;
